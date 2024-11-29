8e04e2dbc6d29b1e2e06b3cc00a6dbfa03d818d3
Justbeanpole <dbtjd7282@gmail.com>
2024-11-29 21:48:15 +0900

    feat : 내역 작성란 초기 html 구조 작성

---
+++ src/components/History.jsx
@@ -0,0 +1,14 @@
+const History = () => {
+    return (
+        <div className="History">
+            <input type="checkbox"/>
+            <span>날짜</span>
+            <span>자산</span>
+            <span>분류</span>
+            <span>금액</span>
+            <span>내용</span>
+        </div>
+    )
+}
+
+export default History;
