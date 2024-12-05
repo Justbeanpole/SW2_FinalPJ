f345be136b469cfdb2194c35cd5b13f13aaa39db
Justbeanpole <dbtjd7282@gmail.com>
2024-12-05 18:11:30 +0900

    feat: 메인페이지 제작

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import History from "./components/History";
 import Detail  from "./components/Detail";
+import MainPage from "./pages/MainPage";
 
 function App() {
   return (
       <div className="App">
-        <Detail></Detail>
+            <MainPage></MainPage>
       </div>
   );
 }
