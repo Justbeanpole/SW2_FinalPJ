7e02b39b9c1c299db6be398011c7a19b51b07e94
Justbeanpole <dbtjd7282@gmail.com>
2024-12-06 19:09:45 +0900

    refactor : 페이지 라우팅 수정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
 import './App.css';
-import History from "./components/History";
-import Detail  from "./components/Detail";
 import MainPage from "./pages/MainPage";
+import CalendarPage from "./pages/CalendarPage";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
+import MenuBar from "./components/MenuBar";
 
 function App() {
   return (
       <div className="App">
-            <MainPage></MainPage>
+          <div className="main-page">
+              <Header></Header>
+              <div className="main-content">
+                  <MenuBar/>
+                  <Routes>
+                      <Route path='/' element={<MainPage></MainPage>}></Route>
+                      <Route path='/cal' element={<CalendarPage/>}></Route>
+                  </Routes>
+              </div>
+              <div className="floating-add-btn material-symbols-outlined">add</div>
+          </div>
+
       </div>
   );
 }
