69ac7a8dd9aea045b66b8eeb4e99cdb01be4c4a1
Justbeanpole <dbtjd7282@gmail.com>
2024-12-12 22:10:15 +0900

    feat : 카테고리 클릭 시 정렬사항 표시 기능,  CategoryBar 삭제 후 Detail에 포함

--- src/components/Detail.jsx
+++ src/components/Detail.jsx
@@ -1,24 +1,12 @@
 import History from "./History";
-import InOutBtn from "./InOutBtn";
 import './style/detailsty.css'
+import {useState} from "react";
 
 const Detail = () => {
     return (
         <div className="detail">
-            <InOutBtn />
-            <div className="middleSection">
-                <input type="checkbox"/>
-                <div>날짜</div>
-                <div>자산</div>
-                <div>분류</div>
-                <div>금액</div>
-                <div>내용</div>
-            </div>
+            <CategoryBar></CategoryBar>
             <div className="historySection">
-                {/*<div className="empty-state">*/}
-                {/*    <img src="/empty-icon.png" alt="empty"/>*/}
-                {/*    <p>데이터가 없습니다.</p>*/}
-                {/*</div>*/}
                 <History></History>
                 <History></History>
                 <History></History>
@@ -28,3 +16,56 @@ const Detail = () => {
 }
 
 export default Detail
+
+const CategoryBar = () => {
+    const [activeCategory, setActiveCategory] = useState(null)
+    const [sortDirection, setSortDirection] = useState(null)
+
+    const handleActCategory = (cateName) => {
+        if(activeCategory === cateName) {
+            if (sortDirection === 'asc') {
+                setSortDirection("desc")
+            }
+           else if (sortDirection === 'desc') {
+               setActiveCategory(null)
+               setSortDirection(null)
+            }
+           else{
+               setSortDirection("asc")
+            }
+        }
+        else{
+            setActiveCategory(cateName)
+            setSortDirection("asc")
+        }
+    }
+    const deterArrowDir = (cateName) => {
+        return activeCategory === cateName && (sortDirection === "asc" ? "▲" : "▼")
+    }
+
+    return (
+        <div className="middleSection">
+            <input type="checkbox"/>
+            <div
+                className={activeCategory === "날짜" ? "activeChar" : ""}
+                onClick={()=>handleActCategory("날짜")}>
+                날짜{deterArrowDir("날짜")}</div>
+            <div
+                className={activeCategory === "자산" ? "activeChar" : ""}
+                onClick={()=>handleActCategory("자산")}>
+                자산{deterArrowDir("자산")}</div>
+            <div
+                className={activeCategory === "분류" ? "activeChar" : ""}
+                onClick={()=>handleActCategory("분류")}>
+                분류{deterArrowDir("분류")}</div>
+            <div
+                className={activeCategory === "금액" ? "activeChar" : ""}
+                onClick={()=>handleActCategory("금액")}>
+                금액{deterArrowDir("금액")}</div>
+            <div
+                className={activeCategory === "내용" ? "activeChar" : ""}
+                onClick={()=>handleActCategory("내용")}>
+                내용{deterArrowDir("내용")}</div>
+        </div>
+    )
+}
