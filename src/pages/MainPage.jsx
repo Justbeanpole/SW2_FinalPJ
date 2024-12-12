f0235a3a532df260b7e4b357b798f1e87b31401a
Justbeanpole <dbtjd7282@gmail.com>
2024-12-12 22:15:54 +0900

    feat : 검색 클릭 시 검색 영역 표현, 달 넘기기 기능

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,31 +1,62 @@
 import Detail from '../components/Detail';
 import './pageSty/MP.css'
-import Header from '../components/Header'
-import MenuBar from "../components/MenuBar";
+import InOutBtn from "../components/InOutBtn";
+import {useState} from "react";
+import {format} from "date-fns";
 
+const MainPage = ({currentMonth, prevMonth, nextMonth, nowMonth}) => {
 
-const MainPage = () => {
     return (
         <div className="mainSection">
-            <div className="top-section">
-                <div className="right-section">
-                    <div className="right-content">
-                                <span className="material-symbols-outlined arrow-btn">
-                                    chevron_left
-                                </span>
-                        <div className="btn month-btn">2024-12</div>
-                        <span className="material-symbols-outlined arrow-btn">
-                                    chevron_right
-                                </span>
-                    </div>
-                    <div className="btn current-month-btn">이번달</div>
-                </div>
-                <div className="btn-search">
-                    <button className="search-btn material-symbols-outlined">search</button>
-                </div>
-            </div>
+            <TopSection
+                currentMonth={currentMonth}
+                nextMonth={nextMonth}
+                prevMonth={prevMonth}
+                nowMonth={nowMonth}
+            ></TopSection>
+            <InOutBtn/>
             <Detail></Detail>
         </div>
     )
 }
 export default MainPage
+
+const TopSection = ({currentMonth, prevMonth, nextMonth, nowMonth}) => {
+    const [openSearchInput, setOpenSearchInput] = useState(false)
+
+    const handleSearchInput = () => {
+        openSearchInput ? setOpenSearchInput(false) : setOpenSearchInput(true)
+    }
+
+    return (
+        <div className="top-section">
+            <div className="right-section">
+                <div className="right-content">
+                    <span
+                        className="material-symbols-outlined arrow-btn"
+                        onClick={prevMonth}>
+                        chevron_left
+                    </span>
+                    <div className="btn month-btn">
+                        {format(currentMonth, 'yyyy')}-{format(currentMonth, 'MM')}
+                    </div>
+                    <span
+                        className="material-symbols-outlined arrow-btn"
+                        onClick={nextMonth}>
+                        chevron_right
+                    </span>
+                </div>
+                <div className="btn current-month-btn" onClick={nowMonth}>이번달</div>
+            </div>
+            <div
+                className="btn-search"
+            >
+                {openSearchInput ? <input type="text" className="search-input"/> : null}
+                <button
+                    className="search-btn material-symbols-outlined"
+                    onClick={handleSearchInput}
+                >search</button>
+            </div>
+        </div>
+    )
+}
