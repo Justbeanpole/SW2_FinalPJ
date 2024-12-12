008cc9fef81749bb084c64b1f4113f809b3145e3
Justbeanpole <dbtjd7282@gmail.com>
2024-12-12 22:12:26 +0900

    feat : 페이지에 맞는 헤더 이름 지정 기능

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,20 @@
 import './style/headerSty.css'
+import {useEffect, useState} from "react";
 
-const Header = () => {
+const Header = ({location}) => {
+    const [headerName, setHeaderName] = useState('')
+    useEffect(() => {
+        switch (location.pathname) {
+            case '/': setHeaderName('내역'); break;
+            case '/cal': setHeaderName('달력'); break;
+            case '/statistics': setHeaderName('통계'); break;
+            default: setHeaderName("")
+        }
+    })
     return (
         <div className="main-header">
-            <div className="menu-btn material-symbols-outlined">menu</div>
-            <div className="header-content">내역</div>
+            <div className="menu-btn material-symbols-outlined">savings <span className="title-name">가계부</span></div>
+            <div className="header-content">{headerName}</div>
             <div className="material-symbols-outlined account">account_circle</div>
         </div>
     )
