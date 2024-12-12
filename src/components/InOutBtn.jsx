f1ecf79d11f849c956546114aa907495cb5e7f43
Justbeanpole <dbtjd7282@gmail.com>
2024-12-12 22:14:05 +0900

    feat : 클릭에 따른 아래선 이동 설정

--- src/components/InOutBtn.jsx
+++ src/components/InOutBtn.jsx
@@ -1,17 +1,43 @@
 import './style/inOutSty.css'
+import {useState} from "react";
 
 const InOutBtn = () => {
+    const [activeTab, setActiveTab] = useState('전체')
+
+    const handleTabChange = (type) => {
+        setActiveTab(type)
+    }
+
     return (
         <div className="topSection">
-            <div className="summary total">
+            <div
+                className={`summary total ${activeTab === '전체' ? 'active' : ''}`}
+                onClick={() => handleTabChange("전체")}>
                 전체 <span>0원</span>
             </div>
-            <div className="summary income">
+            <div
+                className={`summary income ${activeTab === '수입' ? 'active' : ''}`}
+                onClick={() => handleTabChange("수입")}
+            >
                 수입 <span>0원</span>
             </div>
-            <div className="summary expense">
+            <div
+                className={`summary expense ${activeTab === '지출' ? 'active' : ''}`}
+                onClick={() => handleTabChange("지출")}
+            >
                 지출<span>0원</span>
             </div>
+            <div
+                className="underline"
+                style={{
+                    transform: `translateX(${
+                        activeTab === '전체'
+                            ? "-100%"
+                            : activeTab === "수입" ? "0%"
+                                : activeTab === "지출" ? "100%"
+                                    : "-100%"})`,
+                }}
+            ></div>
         </div>
     )
 }
