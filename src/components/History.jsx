a8dc428df61b91a252f81becdca4f422f2939b39
Justbeanpole <dbtjd7282@gmail.com>
2024-12-12 22:13:27 +0900

    style : 주석 삭제

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -15,16 +15,6 @@ import './style/historySty.css'
 
 const History = () => {
     return (
-        // <div className="history-container">
-        //     <div className="history-header">
-        //         <input type="checkbox" className="header-checkbox"/>
-        //         <span className="header-date">날짜</span>
-        //         <span className="header-asset">자산</span>
-        //         <span className="header-category">분류</span>
-        //         <span className="header-amount">금액</span>
-        //         <span className="header-content">내용</span>
-        //     </div>
-        // </div>
         <div className="history-row">
             <input type="checkbox" className="row-checkbox"/>
             <div className="row-date">24. 12. 5. (목)</div>
@@ -32,7 +22,6 @@ const History = () => {
             <div className="row-category">文化 문화생활</div>
             <div className="row-amount">123,123원</div>
         </div>
-
     );
 };
 
