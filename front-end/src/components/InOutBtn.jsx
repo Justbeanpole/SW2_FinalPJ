00bbe7c1f0f82b80b333bcc4259c9857eefbb2a1
Justbeanpole <dbtjd7282@gmail.com>
2024-12-14 21:47:08 +0900

    feat : 하루치, 달별 각 지출,수입,전체 별 총합 표현 기능

--- front-end/src/components/InOutBtn.jsx
+++ front-end/src/components/InOutBtn.jsx
@@ -1,40 +1,40 @@
 import './style/inOutSty.css'
 import {useState} from "react";
 
-const InOutBtn = () => {
-    const [activeTab, setActiveTab] = useState('전체')
-
-    const handleTabChange = (type) => {
-        setActiveTab(type)
-    }
-
+const InOutBtn = ({totalCount, activeTab, handleTabChange}) => {
     return (
         <div className="topSection">
             <div
-                className={`summary total ${activeTab === '전체' ? 'active' : ''}`}
-                onClick={() => handleTabChange("전체")}>
-                전체 <span>0원</span>
+                className={`summary total ${activeTab === 'total' ? 'active' : ''}`}
+                onClick={() => handleTabChange("total")}>
+                전체 <span>
+                {totalCount.total.toLocaleString()}
+                원</span>
             </div>
             <div
-                className={`summary income ${activeTab === '수입' ? 'active' : ''}`}
-                onClick={() => handleTabChange("수입")}
+                className={`summary income ${activeTab === 'out' ? 'active' : ''}`}
+                onClick={() => handleTabChange("in")}
             >
-                수입 <span>0원</span>
+                수입 <span>
+                {totalCount.incomeTotal.toLocaleString()}
+                원</span>
             </div>
             <div
-                className={`summary expense ${activeTab === '지출' ? 'active' : ''}`}
-                onClick={() => handleTabChange("지출")}
+                className={`summary expense ${activeTab === 'out' ? 'active' : ''}`}
+                onClick={() => handleTabChange("out")}
             >
-                지출<span>0원</span>
+                지출<span>
+                {totalCount.expenseTotal.toLocaleString()}
+                원</span>
             </div>
             <div
                 className="underline"
                 style={{
                     transform: `translateX(${
-                        activeTab === '전체'
+                        activeTab === 'total'
                             ? "-100%"
-                            : activeTab === "수입" ? "0%"
-                                : activeTab === "지출" ? "100%"
+                            : activeTab === "in" ? "0%"
+                                : activeTab === "out" ? "100%"
                                     : "-100%"})`,
                 }}
             ></div>
